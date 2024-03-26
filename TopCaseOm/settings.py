06db65221fdd9571d1864f22baf543c79eb2fbef
om <omchik86@mail.ru>
2024-03-26 10:19:23 +0300

    oshibka v baze

--- TopCaseOm/settings.py
+++ TopCaseOm/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'TopCaseOm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TopCaseOm',
+        'USER': 'TopCaseOm',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
