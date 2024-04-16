c63b5a64a5caccf34e5612723f9567553afbaa8f
om <omchik86@mail.ru>
2024-04-16 12:21:18 +0300

    16.04 Postgresql data base connected and full text search
    Signed-off-by: om <omchik86@mail.ru>

--- TopCaseOm/settings.py
+++ TopCaseOm/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     'debug_toolbar',
 
@@ -63,7 +64,7 @@ ROOT_URLCONF = 'TopCaseOm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
