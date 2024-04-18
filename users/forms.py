38975099dad1261afd264f3651e78438542b1820
om <omchik86@mail.ru>
2024-04-18 12:39:07 +0300

    18.04 Login form and controller are ready
    Signed-off-by: om <omchik86@mail.ru>

--- users/forms.py
+++ users/forms.py
@@ -4,20 +4,11 @@ from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(
-        label = 'Имя',
-        widget=forms.TextInput(attrs={'autofocus': True,
-                                      'class': 'form-control',
-                                      'placeholder': 'Введите Ваше имя пользователя'})
-    )
-    password = forms.CharField(
-        label = 'Пароль',
 
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
-                                          'class': 'form-control',
-                                          'placeholder': 'Введите Ваш пароль'
-                                          })
-    )
     class Meta:
         model = User
         fields = ['username', 'password']
+
+    username = forms.CharField()
+    password = forms.CharField()
+
